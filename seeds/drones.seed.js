81223262cbeefb2da2d50246d8ecfef1335fc65c
Marcelus <kz_@hotmail.es>
2022-02-11 17:24:27 +0100

    Iteration 1

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,19 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+require('../db/index');
+
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+Drone.create(drones)
+    .then(dronesOnDB => {
+        console.log(`We have created ${dronesOnDB.length} drones`);
+    })
+    .catch((err) => console.log("Error creating drones", err))
+
